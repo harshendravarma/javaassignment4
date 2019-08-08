1d4e8579410352f2cf58ef35092a7fb4886be749
harshendravarma <47440162+harshendravarma@users.noreply.github.com>
2019-08-08 13:31:14 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,9 +4,10 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-class A{
+class Datevalidator{
 	public void getkycdateranges(String signupdatestring,String formdatestring){
 		try {
+			//convert given string to date
 			SimpleDateFormat signupdateformatter=new SimpleDateFormat("dd-MM-yyyy");
 			Date signupdate=signupdateformatter.parse(signupdatestring);
 			SimpleDateFormat formdateformatter=new SimpleDateFormat("dd-MM-yyyy");
@@ -15,6 +16,7 @@ class A{
 				System.out.println("No range");
 			}
 			else {
+				//take current year signup dates date and month
 				 Date fromdate=signupdate;
 				 fromdate.setYear(formdate.getYear());
 				 Calendar cal = Calendar.getInstance();
@@ -23,9 +25,11 @@ class A{
 				 fromdate = cal.getTime(); 
 				 cal.add(Calendar.DATE, 60);
 				 Date todate = cal.getTime();
+				 //to date is after formdate the todate is formdate
 				 if(todate.after(formdate)) {
 					 todate=formdate;
 				 }
+				 //convert calender format to string
 				 String fromdateoutputstring = signupdateformatter.format(fromdate);
 				 String todateoutputstring = signupdateformatter.format(todate);      
 				 System.out.print(fromdateoutputstring);
@@ -39,7 +43,7 @@ class A{
 }
 public class Main {
 	public static void main(String[] args) {
-		A a=new A();
+		Datevalidator a=new Datevalidator();
 		a.getkycdateranges("16-07-1998","27-06-2017");
 		a.getkycdateranges("04-02-2016","04-04-2017");
 		a.getkycdateranges("04-05-2017","04-04-2017");
