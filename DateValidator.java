e33a36ccdd86a5fcbe7baa36c16ea4be65f85f0d
harshendravarma <47440162+harshendravarma@users.noreply.github.com>
2019-09-18 14:29:07 +0530

    Add files via upload

--- DateValidator.java
+++ DateValidator.java
@@ -5,11 +5,13 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class DateValidator {
+	
 	static Date signupDate, formDate;
 	static SimpleDateFormat signUpDateFormatter, formDateFormatter;
-
+	
+	/* convert given string to date */
 	public static void convertStringToDate(String signUpDateString, String formDateString) {
-		/* convert given string to date */
+		
 		try {
 			signUpDateFormatter = new SimpleDateFormat("dd-MM-yyyy");
 			signupDate = signUpDateFormatter.parse(signUpDateString);
@@ -18,42 +20,50 @@ public class DateValidator {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
-
+	
+	/* convert calendar format to string */
 	public static void convertDateToString(Date fromDate, Date toDate) {
-		/* convert calendar format to string */
+		
 		String fromDateOutputString = signUpDateFormatter.format(fromDate);
 		String toDateOutputString = signUpDateFormatter.format(toDate);
 		printOutputDates(fromDateOutputString, toDateOutputString);
+		
 	}
-
+	
+	/* prints the output */
 	public static void printOutputDates(String fromDateOutputString, String toDateOutputString) {
-		/* prints the output */
+	
 		System.out.print(fromDateOutputString);
 		System.out.print(" ");
 		System.out.println(toDateOutputString);
+		
 	}
 
 	public static void getKycDateRanges(String signUpDateString, String formDateString) {
+		
 		convertStringToDate(signUpDateString, formDateString);
 		try {
 			if (signupDate.after(formDate)) {
 				System.out.println("No range");
 			} else {
-				/* take current year signUp dates date and month */
+				
 				Date fromDate = signupDate;
-				fromDate.setYear(formDate.getYear());
+				fromDate.setYear(formDate.getYear());//get year from formdate for fromdate
 				Calendar cal = Calendar.getInstance();
 				cal.setTime(fromDate);
-				cal.add(Calendar.DATE, -30);
+				cal.add(Calendar.DATE, -30);//subtracts 30 days from current date to get from date
 				fromDate = cal.getTime();
-				cal.add(Calendar.DATE, 60);
+				cal.add(Calendar.DATE, 60);//adds 60 days to fromdate to get todate 
 				Date toDate = cal.getTime();
+				
 				/* if toDate is after formDate the toDate is formDate */
 				if (toDate.after(formDate)) {
 					toDate = formDate;
 				}
-				convertDateToString(fromDate, toDate);
+	
+				convertDateToString(fromDate, toDate);// converts date back to string to print
 
 			}
 		} catch (Exception e) {
@@ -62,10 +72,12 @@ public class DateValidator {
 	}
 
 	public static void main(String[] args) {
-		getKycDateRanges("16-07-1998", "27-06-2017");
-		getKycDateRanges("04-02-2016", "04-04-2017");
-		getKycDateRanges("04-05-2017", "04-04-2017");
-		getKycDateRanges("04-04-2015", "04-04-2016");
-		getKycDateRanges("04-04-2015", "15-03-2016");
+		
+		getKycDateRanges("16-07-1998", "27-06-2017"); //output: 16-06-2017 27-06-2017
+		getKycDateRanges("04-02-2016", "04-04-2017"); //output: 05-01-2017 06-03-2017
+		getKycDateRanges("04-05-2017", "04-04-2017"); //output: No range
+		getKycDateRanges("04-04-2015", "04-04-2016"); //output: 05-03-2016 04-04-2016
+		getKycDateRanges("04-04-2015", "15-03-2016"); //output: 05-03-2016 15-03-2016
+		
 	}
 }
